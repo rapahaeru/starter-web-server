7ee9b77a348da85461b41ece19b867f53e49fe2c
rapahaeru <rapahaeru@gmail.com>
2018-08-09 15:03:48 -0300

    implements repository structure with JSON's response with statusCode and content-type

---
+++ api/email_repository.go
@@ -0,0 +1,25 @@
+package api
+
+import "encoding/json"
+
+// Email payload's pattern to mount the Json of each mail
+type Email struct {
+	ID                   int    `json:"id"`
+	Name                 string `json:"name"`
+	Description          string `json:"description"`
+	Img                  string `json:"img"`
+	DateDelivery         string `json:"date_delivery"`
+	DateDeliveryFormated string `json:"date_delivery_formated"`
+}
+
+// GetEmailList é mto legal
+func GetEmailList() ([]byte, error) {
+	data := []Email{
+		{1, "Cael", "Descrição do cael", "/assets/images/hyoga.gif", "2018-06-25", "25/06/2018"},
+		{2, "ueda", "Descrição do Ueda", "/assets/images/seiya.jpg", "2018-06-20", "20/06/2018"},
+		{2, "celestino", "Descrição do Celestino", "/assets/images/shun.png", "2018-05-24", "24/05/2018"},
+		{2, "vasco", "Descrição do Vasco", "/assets/images/vegeta.jpg", "2018-03-15", "15/03/2018"},
+	}
+
+	return json.Marshal(data)
+}
--- go-server.go
+++ go-server.go
@@ -1,59 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/rapahaeru/starter-web-server/api"
 )
 
 func main() {
 
 	Router := chi.NewRouter()
-	Router.Get("/", showPage)
+	Router.Get("/", handler)
 
 	http.ListenAndServe(":9090", Router)
 }
 
-type Emails struct {
-	ID                   int    `json:"id"`
-	Name                 string `json:"name"`
-	Description          string `json:"description"`
-	Img                  string `json:"img"`
-	DateDelivery         string `json:"date_delivery"`
-	DateDeliveryFormated string `json:"date_delivery_formated"`
-}
-
-func showPage(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() // parse arguments, you have to call this by yourself
-	// fmt.Println(r)                                 // shows entire request
-	fmt.Println("form info server side: ", r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["algumacoisa"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
-
-	json, _ := responseJSON()
-
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json, _ := api.GetEmailList()
 	w.Write(json)
 
 	fmt.Println("json : ", string(json))
-}
-
-func responseJSON() ([]byte, error) {
-	//https://tour.golang.org/moretypes/20
-	data := []Emails{
-		{1, "Cael", "Descrição do cael", "/assets/images/hyoga.gif", "2018-06-25", "25/06/2018"},
-		{2, "ueda", "Descrição do Ueda", "/assets/images/seiya.jpg", "2018-06-20", "20/06/2018"},
-		{2, "celestino", "Descrição do Celestino", "/assets/images/shun.png", "2018-05-24", "24/05/2018"},
-		{2, "vasco", "Descrição do Vasco", "/assets/images/vegeta.jpg", "2018-03-15", "15/03/2018"},
-	}
-
-	return json.Marshal(data)
 }
